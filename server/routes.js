cdaffb0005ec77bdcf1b11cd950c325f86800091
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2020-03-21 23:05:47 +0100

    remove public folder

--- server/routes.js
+++ server/routes.js
@@ -3,4 +3,8 @@ import casesRouter from './api/controllers/cases/router';
 export default function routes(app) {
   app.use('/api/v1/cases', casesRouter);
   app.use('/api/v1', (req, res, next) => res.json({ message: 'API is working' }));
+  app.use('/', (req, res, next) => res.json({
+    title: 'CODIV-19 Report API',
+    message: 'Version 1'
+  }));
 }
